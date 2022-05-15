db2c62867427490b51691b029a8bd67daf9021b8
rsobt <iida_t@eis.hokudai.ac.jp>
2022-05-15 06:58:57 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# VQGAN_Japanese-CLOOB
+
+## About this repository
+rinnaが公開した日本語版CLIPとVQGANを用いた日本語からの画像自動生成を行う。
+
+Automatic generation of images from Japanese with CLIP in Japanese published by rinna and VQGAN.
+
+## Preparation
+```bash
+bash init.sh
+```
+
+## Generated sample images
+
+## Reference
+* [rinna社、日本語に特化した言語画像モデルCLIPを公開](https://prtimes.jp/main/html/rd/p/000000031.000070041.html)
+* [Hugging Face(rinna/japanese-cloob-vit-b-16)](https://huggingface.co/rinna/japanese-cloob-vit-b-16)
+* [VQE + CLIP(English)(tweet)](https://twitter.com/ak92501/status/1421246864649773058)
---
+++ init.sh
@@ -0,0 +1,9 @@
+pip install git+https://github.com/rinnakk/japanese-clip.git  &> /dev/null
+git clone https://github.com/CompVis/taming-transformers      &> /dev/null
+pip install transformers                                      &> /dev/null
+pip install ftfy regex tqdm omegaconf pytorch-lightning       &> /dev/null
+pip install kornia                                            &> /dev/null
+pip install einops                                            &> /dev/null
+pip install wget                                              &> /dev/null
+pip install pillow==7.1.2                                     &> /dev/null
+echo Installation completed.
