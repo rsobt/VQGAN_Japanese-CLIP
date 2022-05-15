67088427ce75ab7213f84928b1878050ad6678cb
rsobt <iida_t@eis.hokudai.ac.jp>
2022-05-15 08:12:47 +0000

    bug fix

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+pip install --upgrade pip
+pip uninstall torch
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 pip install git+https://github.com/rinnakk/japanese-clip.git
 git clone https://github.com/CompVis/taming-transformers
 pip install transformers
@@ -5,5 +8,6 @@ pip install ftfy regex tqdm omegaconf pytorch-lightning
 pip install kornia
 pip install einops
 pip install wget
-pip install pillow==7.1.2
+pip install pillow
+mkdir steps
 echo Installation completed.
