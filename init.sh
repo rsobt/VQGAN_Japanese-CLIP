92d8a1bcb6c37edf02671bd72518a01f03631c9c
rsobt <iida_t@eis.hokudai.ac.jp>
2022-05-15 07:55:34 +0000

    removeoption

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
-pip install git+https://github.com/rinnakk/japanese-clip.git  &> /dev/null
-git clone https://github.com/CompVis/taming-transformers      &> /dev/null
-pip install transformers                                      &> /dev/null
-pip install ftfy regex tqdm omegaconf pytorch-lightning       &> /dev/null
-pip install kornia                                            &> /dev/null
-pip install einops                                            &> /dev/null
-pip install wget                                              &> /dev/null
-pip install pillow==7.1.2                                     &> /dev/null
+pip install git+https://github.com/rinnakk/japanese-clip.git
+git clone https://github.com/CompVis/taming-transformers
+pip install transformers
+pip install ftfy regex tqdm omegaconf pytorch-lightning
+pip install kornia
+pip install einops
+pip install wget
+pip install pillow==7.1.2
 echo Installation completed.
